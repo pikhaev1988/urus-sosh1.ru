210de6b2ad968c409b47123f7405da726065d3b3
pikhaev1988 <100423743+pikhaev1988@users.noreply.github.com>
2023-02-27 17:39:03 +0300

    er

--- baza_urus/urus_sosh1/views.py
+++ baza_urus/urus_sosh1/views.py
@@ -14,6 +14,7 @@ import requests
 from bs4 import BeautifulSoup
 import mysql.connector
 import json
+from Tools.scripts.parse_html5_entities import create_dict
 
 class regist(CreateView):
     form_class = register
